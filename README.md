bc17fee7ec504c639aa921575af93cfe99f77c06
vatsalsharma376 <vatsalsharma376@gmail.com>
2023-08-23 01:22:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Fullstack Twitter Clone 
+
---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auths.js";
+import tweetRoutes from "./routes/tweets.js";
+
+const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("connect to mongodb database");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tweets", tweetRoutes);
+const PORT = (process.env.PORT || 8000);
+app.listen(PORT, () => {
+  connect();
+  console.log("Listening to port", PORT);
+});
